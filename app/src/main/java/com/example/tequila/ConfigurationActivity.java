5b626e93a49b2e3dfb453eecc68d67d09d758988
Francisco <franciscoj.sparcoltary@gmail.com>
2019-11-06 20:40:49 -0600

    Diseño Alfa Version

--- app/src/main/java/com/example/tequila/ConfigurationActivity.java
+++ app/src/main/java/com/example/tequila/ConfigurationActivity.java
@@ -10,5 +10,7 @@ public class ConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        setTitle("Configuración");
     }
 }
--- app/src/main/java/com/example/tequila/HomeActivity.java
+++ app/src/main/java/com/example/tequila/HomeActivity.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textBienvenida;
-
-    Button btnProfile;
+    ImageButton imgLibro;
+    Button btnProfile, btnConfig, btnLibro;
 
 
     @Override
@@ -19,22 +20,38 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        setTitle("Buscar");
+
         Bundle extras = getIntent().getExtras();
         String Usuario = extras.getString("valorMatricula");
 
         textBienvenida = findViewById(R.id.textViewBuscar);
-        textBienvenida.setText("Bienvenido alumno con matricula " + Usuario);
+        textBienvenida.setText("Tu matricula: " + Usuario);
 
         btnProfile = findViewById(R.id.buttonPerfil);
+        btnConfig = findViewById(R.id.buttonConfiguracion);
+        btnLibro = findViewById(R.id.buttonLibro);
+
         btnProfile.setOnClickListener(this);
+        btnLibro.setOnClickListener(this);
+        btnConfig.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
 
+        if(view.getId() == R.id.buttonPerfil){
+            Intent intentProfile = new Intent(this, ProfileActivity.class);
+            startActivity(intentProfile);
+        } else if (view.getId() == R.id.buttonConfiguracion){
+            Intent intentConfiguration = new Intent(this, ConfigurationActivity.class);
+            startActivity(intentConfiguration);
+        } else if (view.getId() == R.id.buttonLibro) {
+            Intent intentBook = new Intent (this, LibroActivity.class);
+            startActivity(intentBook);
+        }
 
-        Intent intentProfile = new Intent(this, ProfileActivity.class);
-        startActivity(intentProfile);
+
 
     }
 }
